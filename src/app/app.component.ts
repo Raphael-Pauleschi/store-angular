09342766f71b25d287f7694e4a02a16e43903c6f
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-10-09 13:24:35 -0300

    project-setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'psn-store-interface-clone-angular';
+  title = 'store-angular';
 }
